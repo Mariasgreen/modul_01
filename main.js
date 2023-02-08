3024e84712abda3d328c79bd910702f2f8c84789
mariiasgreen <mariiaz555@gmail.com>
2023-02-08 11:21:24 +0200

    changed

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+{
+  const item = 'smart проектор philips';
+  const count = 15;
+  const category = 'видео техника';
+  const price = 1200;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+    const thing = 'smart watch';
+    const count = 5;
+    const category = 'accesories';
+    const price = 500;
+
+    console.log(thing);
+    console.log(`общая сумма товара: ${count * price}`);
+
+}
