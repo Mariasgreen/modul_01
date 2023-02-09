03e498c671b922c06fd6125903e2d8915bf4d806
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-02-09 15:02:56 +0200

    Add files via upload

---
+++ einstein.js
@@ -0,0 +1,6 @@
+const catWeight = 4;
+const speedLight = (3*10)**8;
+
+const e = (catWeight*speedLight)**2 ;
+
+console.log(e);
--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 'use strict'
 
-{
-  const item = 'smart проектор philips';
-  const count = 15;
-  const category = 'видео техника';
-  const price = 1200;
 
-  console.log(item);
-  console.log(`общая сумма товара: ${count * price}`);
-}
 
-{
-    const thing = 'smart watch';
-    const count = 5;
-    const category = 'accesories';
-    const price = 500;
 
-    console.log(thing);
-    console.log(`общая сумма товара: ${count * price}`);
+const item = prompt('Наименование товара') ;
+const count = prompt('Количество товара') ;
+const category = prompt('Категория товара') ;
+const price = prompt('Цена товара') ;
 
-}
+
+const number = Number(count);
+const secnumber =  Number(price);
+
+
+  console.log(typeof item);
+  console.log(typeof number);
+  console.log(typeof category);
+  console.log(typeof secnumber);
+
+
+  console.log('На складе ' + count + ' единицы товара ' + item + ` на сумму товара: ${count * price}`  + ' деревянных' ) ;
+
+
+  
