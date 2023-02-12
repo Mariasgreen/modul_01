34e8ea6bc80894aa3d02fd869a30338c1c0cba6a
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-02-12 20:48:50 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 
 
-const result = Number(prompt('число'));
 
 
-  if (result == NaN) {
-    console.log(typeof result);
-    } else {
-        console.log("Вы ввели некорректные данные");
-    }
+
+
+
+ const item = prompt('Наименование товара') ;
+const count = Number(prompt('Количество товара'))
+const category = prompt('Категория товара') ;
+const price =  Number(prompt('Цена товара'))
+
+
+
+
+
+if (isNaN(count) ){
+  console.log("Вы ввели некорректные данные");
+} else  {
+
+  console.log(count);
+
+}
+
+if (isNaN(price) ){
+  console.log("Вы ввели некорректные данные");
+} else  {
+
+  console.log(price);
+
+}
+
+
+
+
+
+
 
