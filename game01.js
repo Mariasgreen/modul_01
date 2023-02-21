538811215e08d2f35a43d47a01600751b95172c6
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-02-21 12:28:48 +0200

    Add files via upload

--- game01.js
+++ game01.js
@@ -2,32 +2,30 @@
 
 
 function guessNumber() {
-    let random = Math.floor(Math.random() * 100);
+    let random = Math.floor(Math.random() * 101);
 
  while (true) {
 
-        let input = +prompt('Введите число от 0 до 100:');
-
-
-        if (isNaN(input)){
-            alert('Введи число!'); 
+        let input = prompt('Введите число от 0 до 100:');
+        if (isNaN(+input)) {
+          alert('Введи число!');
+       
         }
+    if (input=== null){
+      break
+    }
 
-        if (input === null) {
-            alert('Отмена');
-            break;
-          }
+      
 
        else {
-        if (input > random) {
+        if (+input > random) {
             alert('Меньше!');
-          } else if (input < random) {
+          } else if (+input < random) {
             alert('Больше!');
-          } else if (input === random) {
+          } else if (+input === random) {
             alert('Правильно!');
-            break;
+           
           }
-        
         }
          
     }
