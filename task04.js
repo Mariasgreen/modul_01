97efd33c63a457c6cea87c774381df5afbb51e28
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-02-22 15:27:26 +0200

    Add files via upload

--- task04.js
+++ task04.js
@@ -1,28 +1,19 @@
 
+const chooseYears = (start, end) => {
+
+  const yearArr = [];
+  if (end < start) {
+    return chooseYears(end, start)
+  }
+
+  for (let i = start; i <= end; i++) {
+    if ((i % 100 && !(i % 4)) || !(i % 400)) {
+      yearArr.push(i)
+    }
+  }
+   return yearArr
+}
 
 
 
-      const chooseYears = ( start,end) => {
-   
-        const yearArr = [];
-    
-    
-        for (let i = start; i <= end; i++ ) {
-            
-      
-
-          if ((i % 100 && !(i % 4)) || !(i % 400)){
-            yearArr.push(i)
-          }
-
-         
-         
-        }
-      
-
-        return yearArr
-      }
-    
-    
-   
-          console.log(chooseYears (2000,2018))
+console.log(chooseYears(2018, 2000))
