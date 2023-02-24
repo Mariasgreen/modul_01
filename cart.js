57f6f56db636e12bda24fbff2477b0e05f71dfec
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-02-24 17:43:13 +0200

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,48 @@
+'use strict';
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+
+  get TotalPrice() {
+    return this.calculateItemPrice();
+  },
+
+  add(item, price, itemCount = 1) {
+    const addItems = {
+      item,
+      price,
+      itemCount,
+    };
+
+    this.items.push(addItems);
+    this.increaseCount(itemCount);
+  },
+
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(`total : ${this.TotalPrice} $`);
+  },
+
+  calculateItemPrice() {
+    return this.items.reduce((acc, items) =>
+      acc + (items.price * items.itemCount), 0);
+  },
+
+  increaseCount(itemCount) {
+    return this.count += itemCount;
+  },
+
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+};
+
+
+cart.add('jeans', 120, 2);
+cart.add('skirts', 120, 3);
+cart.add('jackets', 120, 4);
+
+cart.print();
