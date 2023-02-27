6acc3fea324c031324323ca2685054721a04ecc8
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-02-27 15:56:36 +0200

    Add files via upload

--- game01.js
+++ game01.js
@@ -1,28 +1,28 @@
+/* eslint-disable require-jsdoc */
 
 'use strict';
 
 
 const random = Math.floor(Math.random() * 101);
 
-
-function guess(input){
-  input = prompt ('Введите число от 0 до 100:');
-  if (isNaN (+input)) {
-if (input === null) {
-  alert('done')
-}
-if (input > random) {
-  alert('Меньше!');
-} else if (input < random) {
-  alert('Больше!');
-} else if (input === random) {
-  alert('Правильно!');
-  return input;
-}  else {
-  alert('Можно вводить только числа');
-}
-return guess()
-}
+function guess(input) {
+  input = prompt('Введите число от 0 до 100:');
+  if (input === null) {
+    return;
+  } if (!isNaN(+input)) {
+    if (+input > random) {
+      alert('Меньше!');
+    } else if (+input < random) {
+      alert('Больше!');
+    } else if (+input === random) {
+      alert('Правильно!');
+      return;
+    }
+  } else {
+    alert('Можно вводить только числа');
+  }
+  return guess();
 }
 
-console.log(guess());
+
+guess();
