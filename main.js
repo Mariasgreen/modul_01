7546cea5f3b3119e6ffd09a30d1f11ba0c940bc8
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-03-03 20:20:14 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const three = document.querySelectorAll('.props__item_three');
 const four = document.querySelectorAll('.props__item_four');
 const five = document.querySelectorAll('.props__item_five');
 const six = document.querySelectorAll('.props__item_six');
-
+const adv = document.getElementsByClassName('ads')
 
 const title = document.querySelectorAll('.item__title');
 
@@ -18,13 +18,26 @@ four[3].before(four[5]);
 
 two[7].after(two[8]);
 two[8].after(two[9]);
-
+adv[0].remove()
 
 document.querySelector('.item_five .content').append(document.querySelector('.item_six .item__title'));
 document.querySelector('.item_two .props__list').before(document.querySelector('.item_five .item__title'));
 document.querySelector('.item_six .props__list').before(document.querySelector('.item_two .item__title'));
 
 
+
+
+//const elem =document.createTextNode('trololo')
+
+const h2 = document.createElement('h2')
+h2.classList.add('item__title')
+
+const text =document.createTextNode('This и прототипы объектов')
+
+
+h2.append(text)
+
+title[2].replaceWith(h2)
 
 
 
@@ -35,3 +48,5 @@ document.querySelector('.item_five .content').append(document.querySelector('.it
 console.log(title);
 
 console.log();
+
+
